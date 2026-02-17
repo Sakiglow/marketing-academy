14d88242e73a75f27583609c79ba1686ca59391a
Sakiglow <alex@admt.ch>
2026-02-17 11:50:26 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MarketingAcademy from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MarketingAcademy />
+  </React.StrictMode>,
+)
